7417ba687ba5359f94547be4e9c94396f6d65a37
saksheekirans25 <saksheekirans@gmail.com>
2024-04-28 17:24:52 -0500

    update color contrast

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
 "use strict";!function(){var n=$("html"),t=function(){$(".btn-menu").on("click",function(t){t.preventDefault(),n.toggleClass("menu-opened")})},e=function(){t()};e()}();
+
+// main.js
